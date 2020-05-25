53f863cffbf9ac7235e706b08ef4a03916054544
jruss <jrusso44@bu.edu>
2020-05-25 10:10:13 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fragmentexampleone/FragmentOne.java
@@ -0,0 +1,32 @@
+package com.example.fragmentexampleone;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Bundle;
+
+public class FragmentOne extends Fragment {
+
+    public FragmentOne() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+}
